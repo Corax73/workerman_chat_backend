0e394b2e4ea4f0f4528ee44b191300d9cd8e9d3c
Corax73 <ramaz999@inbox.ru>
2024-11-04 18:27:27 +0400

    added separated method

--- src/WorkermanWrap.php
+++ src/WorkermanWrap.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Wrap;
 
 use Workerman\Worker;
@@ -11,41 +13,72 @@ class WorkermanWrap extends Worker
     {
         parent::__construct($socket_name);
         $this->count = $this->countConnections;
-        $this->onMessage = function (TcpConnection $connection, string $data) {
+        $this->onMessage = $this->getOnMessageCallback();
+        $this->onConnect = $this->getOnConnectCallback();
+    }
+
+    protected function getOnMessageCallback(): callable
+    {
+        return  function (TcpConnection $connection, string $data) {
+
             if (!empty($data) && $arData = json_decode($data, true)) {
                 if (
                     isset($arData['room_id']) &&
                     $arData['room_id'] &&
                     isset($arData['msg']) &&
-                    $arData['msg'] &&
-                    !isset($arData['go_to_room']) &&
-                    isset($this->rooms[$arData['room_id']]) &&
-                    $this->rooms[$arData['room_id']]
+                    !isset($arData['go_to_room'])
                 ) {
-                    foreach ($this->rooms[$arData['room_id']] as $conn) {
-                        $conn->send($arData['msg']);
-                    }
+                    $this->sendToRoom($arData['room_id'], $arData['msg']);
                 } elseif (isset($arData['go_to_room']) && $arData['go_to_room']) {
                     if (!isset($this->rooms[$arData['go_to_room']])) {
                         $this->rooms[$arData['go_to_room']] = [];
+                        $this->rooms[$arData['go_to_room']]['connection_ids'] = [];
                     }
-                    array_push($this->rooms[$arData['go_to_room']], $connection);
-                    var_dump($this->rooms);
+                    array_push($this->rooms[$arData['go_to_room']]['connection_ids'], $connection->id);
                     if (!property_exists($connection, 'rooms')) {
                         $connection->rooms = [];
                     }
                     $connection->rooms[] = $arData['go_to_room'];
                     $connection->send('your room is № ' . $arData['go_to_room']);
-                    foreach ($this->rooms[$arData['go_to_room']] as $conn) {
-                        $conn->send("User $connection->id joined \n");
+                    if (isset($this->rooms[$arData['go_to_room']]['history']) && $this->rooms[$arData['go_to_room']]['history']) {
+                        $connection->send(implode("\n", $this->rooms[$arData['go_to_room']]['history']));
+                    }
+                    foreach ($this->rooms[$arData['go_to_room']]['connection_ids'] as $id) {
+                        $joinedMsg = "User $connection->id joined \n";
+                        $this->rooms[$arData['go_to_room']]['history'][] = $joinedMsg;
+                        if (isset($this->connections[$id]) && $this->connections[$id]) {
+                            $this->connections[$id]->send($joinedMsg);
+                        }
                     }
                 }
             }
         };
-        $this->onConnect = function (TcpConnection $connection) {
+    }
+
+    protected function getOnConnectCallback(): callable
+    {
+        return function (TcpConnection $connection) {
             $this->connections[$connection->id] = $connection;
-            $connection->send('Room list: ' . implode(',', array_keys($this->rooms)) . '\\n');
+            $connection->send('Room list: ' . implode(',', array_keys($this->rooms)));
         };
+    }
+
+    protected function sendToRoom(string|int $roomCode, string|int $msg): void
+    {
+        if (
+            !empty($roomCode) &&
+            !empty($msg) &&
+            isset($this->rooms[$roomCode]) &&
+            $this->rooms[$roomCode]
+        ) {
+            foreach ($this->rooms[$roomCode]['connection_ids'] as $id) {
+                $this->connections[$id]->send($msg);
+            }
+            if (!isset($this->rooms[$roomCode]['history'])) {
+                $this->rooms[$roomCode]['history'] = [];
+            }
+            $this->rooms[$roomCode]['history'][] = $msg;
+        }
     }
 
     public function start()
